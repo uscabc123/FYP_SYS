44a0091a0f6f37169138793a3e14bded2860422b
DESKTOP-LLTV64L\CHE <haieng0902@gmail.com>
2018-12-14 17:25:22 +0800

    Add project files.

---
+++ FYP/Models/IdentityModels.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FYP.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
